f51a70c1039dc223d582622c7e31a051841faba6
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-14 13:05:08 +0300

    Initial commit

---
+++ src/main/resources/liquibase/changesets/V1__init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS books
+(
+    id         BIGSERIAL PRIMARY KEY,
+    isbn       VARCHAR(17)  NOT NULL UNIQUE,
+    title      VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authors
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authored_books
+(
+    id         BIGSERIAL PRIMARY KEY,
+    book_id    BIGINT REFERENCES books (id),
+    author_id  BIGINT REFERENCES authors (id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
