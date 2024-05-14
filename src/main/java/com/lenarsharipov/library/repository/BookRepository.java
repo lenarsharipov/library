3fff4339af5b87ab0c70765f85c0e24022b68837
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-14 13:08:02 +0300

    repository layer added

---
+++ src/main/java/com/lenarsharipov/library/repository/BookRepository.java
@@ -0,0 +1,21 @@
+package com.lenarsharipov.library.repository;
+
+import com.lenarsharipov.library.model.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByOrderByTitle();
+
+    List<Book> findByOrderByIsbn();
+
+    List<Book> findByOrderByAuthoredBooksAuthor();
+
+}
