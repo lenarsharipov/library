2ebbe70f19e18e279c011e51d806f182dfc19544
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-17 20:15:38 +0300

    populate script updated

--- src/main/resources/liquibase/changesets/V2__populate_data.sql
+++ src/main/resources/liquibase/changesets/V2__populate_data.sql
@@ -1,12 +1,12 @@
-INSERT INTO authors(name, created_at, updated_at)
-VALUES ('Leo Tolstoy', now(), now()),
-       ('Michael Bulgakov', now(), now()),
-       ('Alexander Pushkin', now(), now());
+INSERT INTO authors(name, created_at)
+VALUES ('Leo Tolstoy', now()),
+       ('Michael Bulgakov', now()),
+       ('Alexander Pushkin', now());
 
-INSERT INTO books(isbn, title, created_at, updated_at)
-VALUES ('978-2-1234-5680-3', 'Капитанская дочка', now(), now()),
-        ('123-2-1234-4566-3', 'У Лукоморья', now(), now());
+INSERT INTO books(isbn, title, created_at)
+VALUES ('978-2-1234-5680-3', 'Капитанская дочка', now()),
+        ('123-2-1234-4566-3', 'У Лукоморья', now());
 
-INSERT INTO authored_books(book_id, author_id, created_at, updated_at)
-VALUES (1, 3, now(), now()),
-        (2, 3, now(), now());
+INSERT INTO authored_books(book_id, author_id, created_at)
+VALUES (1, 3, now()),
+        (2, 3, now());
