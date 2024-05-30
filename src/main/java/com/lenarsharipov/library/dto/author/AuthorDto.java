65e2ece204e44b3e194bbbc87eb0675480a31e11
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-30 16:20:22 +0300

    updated according to Many-to-Many

--- src/main/java/com/lenarsharipov/library/dto/author/AuthorDto.java
+++ src/main/java/com/lenarsharipov/library/dto/author/AuthorDto.java
@@ -1,14 +1,8 @@
 package com.lenarsharipov.library.dto.author;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
 
-@Value
 @Builder
-@AllArgsConstructor
-public class AuthorDto {
+public record AuthorDto(Long id, String name) {
 
-    Long id;
-    String name;
 }
